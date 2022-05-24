7aa8f89dc599691c2a3e7c63a32d565dd12e7f0d
JohannnaM <johanna.mattsson@edu.omnia.fi>
2022-05-24 16:14:00 +0300

    otsikon koko

--- -etusivu/index.php
+++ -etusivu/index.php
@@ -29,7 +29,7 @@
             <?php } ?>
 			</div>
 			<div id="ohjesailio">
-				<h3>Uusimmat ohjeet</h3>
+				<h2>Uusimmat ohjeet</h2>
 				<nav id="linkit">
 					<ul><?php
                         $hakusql = "SELECT otsikko, artikkeli_id FROM artikkeli
--- -etusivu/ohje.php
+++ -etusivu/ohje.php
@@ -8,6 +8,7 @@
 		<title>Ohje</title>
 		<link rel="stylesheet" type="text/css" href="tyyli.css">
 		<link href='https://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet'>
+		<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		
 	</head>
